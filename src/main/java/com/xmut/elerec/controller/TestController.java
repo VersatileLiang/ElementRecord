c0193dc9cf53f9cd717d89232957a14386d2d25e
梁楷松 <394605496@qq.com>
2020-03-13 01:59:13 +0800

    简化路径

--- src/main/java/com/xmut/elerec/controller/TestController.java
+++ src/main/java/com/xmut/elerec/controller/TestController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/demoproject/test")
+@RequestMapping("/")
 public class TestController {
 
     @Autowired
     private TestService testService ;
 
-    @RequestMapping(value = "/get/{id}",method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
     public TestEntity test(@PathVariable Integer id){
         System.out.println("id:" + id);
         return testService.getById(id);
